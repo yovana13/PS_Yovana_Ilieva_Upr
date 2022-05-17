9e2be848abd40356d907eafa019efcd00138e7af
yovana13 <73619767+yovana13@users.noreply.github.com>
2022-05-17 20:42:51 +0200

    Add WpfExample

---
+++ WpfExample/RemoveCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class RemoveCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            var nameList = parameter as NamesList;
+            return nameList != null && nameList.SelectedName != null;
+        }
+        public void Execute(object parameter)
+        {
+            var nameList = parameter as NamesList;
+            var oldName = nameList.SelectedName;
+            nameList.Names.Remove(oldName);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
